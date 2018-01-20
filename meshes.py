5d4ca6cd62f62d8c95ed9d8721f8580c0bcf504f
pabolmasov <pavel.abolmasov@gmail.com>
2018-01-20 16:50:37 +0200

    added a flush in rmm output, meshes.py

--- meshes.py
+++ meshes.py
@@ -119,13 +119,13 @@ def musearch(newmdot, newps):
     
 def rmm(newps, neweta):
 
-    d.XQset(0.4, 0.99)
+    d.XQset(1.0, 0.9)
 
     tstart=time.time()
 
-    mumin=20. ;   mumax=100.
-    mdmin=500. ;    mdmax=5000.
-    nmu=10 ;    nd=16
+    mumin=1. ;   mumax=100.
+    mdmin=10. ;    mdmax=10000.
+    nmu=15 ;    nd=16
 
     muar=(mumax/mumin)**(arange(nmu)/double(nmu-1))*mumin
     mdar=(mdmax/mdmin)**(arange(nd)/double(nd-1))*mdmin
@@ -148,27 +148,27 @@ def rmm(newps, neweta):
     for ku in arange(nmu):
         if(ku>0):
             d.XQset(xi[ku-1,0], qeq[ku-1,0]) # the closest value, calculated for the previous row, same column
-
         for kd in arange(nd):
             mu2[ku,kd]=muar[ku]
             md2[ku,kd]=mdar[kd]
             xi[ku,kd],qeq[ku,kd],conv[ku,kd] =d.ordiv_smart(muar[ku], mdar[kd], -newps)
-            if(kd>0):
+            if(conv[ku,kd]):
                 d.XQset(xi[ku,kd], qeq[ku,kd]) # 
             if(not(conv[ku,kd])):
                 xi[ku,kd]=sqrt(-1)
                 qeq[ku,kd]=sqrt(-1)
             print "xi (mu="+str(muar[ku])+", mdot="+str(mdar[kd])+") = "+str(xi[ku,kd])
             #            oin,hin,tin,bmax,mdotint,ll=d.rastr(xi[ku,kd], qeq[ku,kd])
-            oar[ku,kd]=oin
-            har[ku,kd]=hin
-            tauar[ku,kd]=tin
-            betar[ku,kd]=bmax
-            mddotint[ku,kd]=mdotint
-            raam[ku,kd]=raffugen(muar[ku], mdar[kd])
+#            oar[ku,kd]=oin
+#            har[ku,kd]=hin
+#            tauar[ku,kd]=tin
+#            betar[ku,kd]=bmax
+#            mddotint[ku,kd]=mdotint
+#            raam[ku,kd]=raffugen(muar[ku], mdar[kd])
             fout.write(str(muar[ku])+' '+str(mdar[kd])+' '+str(xi[ku,kd])+' '+str(qeq[ku,kd])+'\n ')
                        #str(oin)+' '+str(hin/raam[ku,kd]/xi[ku,kd])+' '+str(tin)+' '+str(bmax)+' '+str(mddotint[ku,kd])+'\n')
             print str(muar[ku])+' '+str(mdar[kd])+' '+str(xi[ku,kd])+' '+str(qeq[ku,kd])+'\n '
+            fout.flush()
     fout.close()
     tend=time.time()
     print "RMM: the grid took "+str(tend-tstart)+"s = "+str((tend-tstart)/3600.)+"h"
